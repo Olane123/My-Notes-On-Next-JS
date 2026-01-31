e8dd8cdfd080658206b5d9ec444f6c585cd8b7a2
Olane123 <olaneyt3@gmail.com>
2026-01-31 12:05:18 +0500

    Project on Next.JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 import Link from "next/link";
+import githubIcon from "./githubicon.png"
 
 const githubLink: string = "https://github.com/Olane123/My-Notes-On-Next-JS";
-const githubImage: string = "/githubicon.png";
 
 export default function Home() {
     return (
@@ -12,7 +12,7 @@ export default function Home() {
                 <h1>Добавляйте, редактируйте или удаляйте записи по мере необходимости.</h1>
 
                 <div className="repository">
-                    <Link href={githubLink}><Image width={100} height={100} src={githubImage} alt="github" /></Link>
+                    <Link href={githubLink}><Image width={100} height={100} src={githubIcon} alt="github" /></Link>
                 </div>
             </div>
         </>
